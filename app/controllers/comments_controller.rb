b61af0634604d490d40bd646cb0f10fe5e2b63f9
Noe Arzate <narzate@gmail.com>
2013-12-09 10:20:20 -0600

    new and changed files

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,15 @@
+class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new(params.require(:comment).permit(:body))
+    @comment.post = @post
+    @comment.creator = User.first #TODO: fix after we have authentication
+
+    if @comment.save
+      flash[:notice] = 'Your comment was added.'
+      redirect_to post_path(@post)
+    else
+      render 'posts/show'
+    end
+  end
+end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,54 @@
 class PostsController < ApplicationController
+  before_action :set_post, only:[:show, :edit, :update]
+
+
   def index
     @posts = Post.all
   end
+
+  def show
+    #@post = Post.find(params[:id])  #we are using before action at the top
+    @comment = Comment.new
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.creator = User.first #TODO: change once we have authentication
+
+    if @post.save
+      flash[:notice] = "Your post was created."
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    #@post = Post.find(params[:id]) #we are using before action at the top
+  end
+
+  def update
+    #@post = Post.find(params[:id])  #we are using before action at the top
+
+    if @post.update(post_params)
+      flash[:notice] =  "This post was updated."
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :url, :description)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 end
+
